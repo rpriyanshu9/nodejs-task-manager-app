313440f9f931f68a6fcf6bc1a2548d695f8402e8
Priyanshu Raj <rpriyanshu9@gmail.com>
2021-07-02 17:51:02 +0530

    added homepage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 require('./db/mongoose')
+const path = require('path')
 const express = require('express')
 const taskRoutes = require('./routes/task')
 const userRoutes = require('./routes/user')
@@ -12,7 +13,11 @@ app.use(express.json())
 // For parsing data sent by a HTML form
 // app.use(express.urlencoded({ extended: true }))
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
+
 // Routes
+app.get('/', (req, res) => res.render('homepage'))
 app.use('/tasks', taskRoutes)
 app.use('/users', userRoutes)
 
