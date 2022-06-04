385b78fd2c91f574a71cd1a2f7d0bf1b75628e98
Priyanshu <rpriyanshu9@gmail.com>
2022-06-04 11:23:20 +0530

    removed email functionality

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,17 +4,12 @@ const router = express.Router();
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 const sharp = require("sharp");
-// const {
-//   sendWelcomeEmail,
-//   sendCancellationEmail,
-// } = require("../emails/account");
 
 // Create
 router.post("/", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    // sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (e) {
@@ -93,7 +88,6 @@ router.patch("/me", auth, async (req, res) => {
 router.delete("/me", auth, async (req, res) => {
   try {
     const deletedUser = await req.user.remove();
-    // sendCancellationEmail(deletedUser.email, deletedUser.name)
     res.send(deletedUser);
   } catch (e) {
     res.status(500).send(e);
